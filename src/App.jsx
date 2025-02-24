8f28d7557e8e815f9189c84bacbdd8dd9f2a0b86
Hrushikesh Kadudula <hrushikesh.kadudula@thoughtclan.com>
2025-02-24 15:24:01 +0530

    tailwind handson

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
@@ -8,24 +7,22 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <h1 class="text-3xl font-bold underline">Hello world!</h1>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className=" flex justify-center items-center min-h-screen">
+        <div className="bg-gray-700 rounded-lg p-8 shadow-2xl max-w-sm w-full">
+          <h2 className="text-center text-3xl text-white mb-6 font-bold">Login</h2>
+          <form action="">
+            <div className="mb-4 ">
+              <label htmlFor="" className="block text-sm font-medium text-gray-300 mb-2">Email</label>
+              <input type="email" className="w-full px-3 py-2 bg-gray-50 rounded-md text-white focus:outline-none focus:ring-blue-500"/>
+            </div>
+            <div className="mb-4">
+              <label htmlFor="" className="block text-sm font-medium text-gray-300 mb-2">Password</label>
+              <input type="password" className="w-full px-3 py-2 bg-gray-50 rounded-md text-white focus:outline-none focus:ring-blue-500"/>
+            </div>
+            <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-700 shadow-lg">login</button>
+          </form>
+        </div>
+    </div>
     </>
   );
 }
